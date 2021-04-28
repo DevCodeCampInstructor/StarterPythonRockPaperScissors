a90418f2086bda5a35b683112a4c083c5fed16a6
Hower16 <miky1216@gmail.com>
2021-04-28 18:20:05 -0500

    Added project files and starter code

---
+++ StarterPythonRPS/main.py
@@ -0,0 +1,30 @@
+import random
+player_one_score = 0
+player_two_score = 0
+
+
+def run_game():
+
+
+def choose_game_type():
+
+
+def set_player_name():
+
+
+def choose_gesture(player_name):
+
+
+def get_random_gesture(player_name):
+
+
+def determine_round_winner(player_one_name, player_one_gesture, player_two_name, player_two_gesture):
+
+
+def display_scoreboard(player_one_name, player_two_name):
+
+
+def check_if_game_winner(player_one_name, player_two_name):
+
+
+run_game()
